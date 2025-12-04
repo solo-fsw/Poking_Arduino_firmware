3a246afe772927da50c68f8de0153d2a1d1c19f5
J den Hartog <JdenHartog@users.noreply.github.com>
2025-12-04 17:55:11 +0100

    Create subs.ino

---
+++ PokingServo/subs.ino
@@ -0,0 +1,45 @@
+void writeStringToEEPROM(int addrOffset, const String &strToWrite)
+{
+  byte len = strToWrite.length();
+  EEPROM.write(addrOffset, len);
+  for (int i = 0; i < len; i++)
+  {
+    EEPROM.write(addrOffset + 1 + i, strToWrite[i]);
+  }
+}
+
+String readStringFromEEPROM(int addrOffset)
+{
+  int newStrLen = EEPROM.read(addrOffset);
+  char data[newStrLen + 1];
+  for (int i = 0; i < newStrLen; i++)
+  {
+    data[i] = EEPROM.read(addrOffset + 1 + i);
+  }
+  data[newStrLen] = '\0'; // the character may appear in a weird way, you should read: 'only one backslash and 0'
+  return String(data);
+}
+
+void DumpMyInfo() { //in JSON format
+  Serial.print("{\"VersionInfo\":\"");
+  Serial.print(VersionInfo);
+  Serial.print("\",");
+  Serial.print("\"Serialnumber\":\"");
+  Serial.print(Serialnumber);
+  Serial.print("\",");
+  Serial.println("\"Device\":\"PokingServo\"}");
+}
+
+void handleCommands() {
+  switch (Serial.read()) {
+    case 'v':
+      DumpMyInfo();
+      break;
+    case 'i':
+      Serial.println(servo.isMoving());
+      break;
+    default:
+      Serial.println("Unknown command");
+      break;
+  }
+}
